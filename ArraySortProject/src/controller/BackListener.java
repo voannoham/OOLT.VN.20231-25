a4c2fa6d7d67a04cb9bee5eed2aa06062c306f20
Nguyen Van Nham <nhampmq@gmail.com>
2024-01-06 21:12:53 +0700

    process event listener

--- ArraySortProject/src/controller/BackListener.java
+++ ArraySortProject/src/controller/BackListener.java
@@ -1,5 +1,18 @@
 package controller;
 
-public class BackListener {
-    
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
+
+import view.MainMenu;
+
+public class BackListener implements ActionListener{
+     @Override
+    public void actionPerformed(ActionEvent event){
+        MainMenu menu = new MainMenu();
+        menu.setVisible(true);
+		SwingUtilities.windowForComponent(((JButton) event.getSource())).dispose();
+    }
 }
--- ArraySortProject/src/controller/HelpListener.java
+++ ArraySortProject/src/controller/HelpListener.java
@@ -1,5 +1,20 @@
 package controller;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
-public class HelpListener {
-    
+public class HelpListener implements ActionListener{
+    @Override
+    public void actionPerformed(ActionEvent event){
+        String helpText ="Sorting is commonly used as the introductory problem \nin various Computer Science classes "
+                    + "to showcase a range of algorithmic ideas\n"
+                    + "Without loss of generality, we assume that we will sort only Integers,\nnot necessarily distinct, in non-decreasing order in this visualization\n";
+        JTextArea textArea = new JTextArea(helpText);
+        JFrame helpFrame = new JFrame("Help");//creat help frame
+        helpFrame.setSize( 640, 360 ); // set frame size
+        helpFrame.setLocationRelativeTo(null);
+        helpFrame.add(textArea);
+        helpFrame.setVisible( true ); // display frame
+    }
 }
